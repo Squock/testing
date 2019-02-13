f333166c5c113d293e9b24b21ba06c13668aedb2
squock <squock@mac-squock.local>
2019-02-13 16:53:50 +0900

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by squock on 13.02.19.
+//  Copyright © 2019 squock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
